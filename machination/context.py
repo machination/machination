7cff1efcbbde72155ec2be22283ffc1dddf4435f
Colin Higgs <colin.higgs@ed.ac.uk>
2012-04-03 19:57:49 +0100

    move config all config information into desired-status, update context

--- machination/context.py
+++ machination/context.py
@@ -15,14 +15,43 @@ __status__ = "Development"
 
 import sys
 import platform
-from os.path import join
+#from os.path import join
+#from os import environ
+import os
 from lxml import etree
 
+desired_status = None
 
 def machination_path():
     "Returns the Machination path"
+
+def status_dir():
+    """returns path to status dir
+
+    usually /var/lib/machination or C:\Program Files\Machination\status"""
+    # try the environment
+    if 'MACHINATION_STATUS_DIR' in os.environ:
+        return os.environ['MACHINATION_STATUS_DIR']
+
+    # now look to see if we've parsed the desired status file
+    if(desired_status):
+        # look it up in desired_status.xml
+        try:
+            directories = desired_status.xpath("/status/directories")[0]
+        except IndexError:
+            raise Exception("/status/directories element not found in '%s'"
+                            % desired_status_file)
+        if "status_dir" in directories.keys():
+            return directories.get("status_dir")
+    
+    # try bootstrapping
+    if 'MACHINATION_BOOTSTRAP_DIR' in os.environ:
+        return os.environ['MACHINATION_BOOTSTRAP_DIR']
+
+    # if all else fails, return the default
     if platform.system()[:3] == "Win":
         try:
+            # look at the registry key HKLM\Software\Machination
             import wmi
             r = wmi.Registry()
             # hDefKey should be _winreg.HKLM but that doesn't work on
@@ -38,20 +67,15 @@ def machination_path():
         except:
             # Something went wrong with the registry setup
             path = 'c:\program files\machination'
+        return os.path.join(path,"status")
     else:
-        #Try/Except block handles the exists/read race condition nicely
-        try:
-            with open("/etc/machination") as f:
-                path = f.readline()
-        except IOError:
-            #File doesn't exist
-            path = '/opt/machination/'
+        return "/var/lib/machination";
 
-    return path
-
-
-config = etree.parse(join(machination_path(), 'config.xml'))
-
+desired_status_file = os.path.join(status_dir(),"desired-status.xml")
+try:
+    desired_status = etree.parse(desired_status_file)
+except IOError:
+    raise IOError("could not find file '%s'" % desired_status_file)
 
 def main(args):
     print(etree.tostring(config).decode('utf8'))
@@ -60,4 +84,4 @@ def main(args):
 if __name__ == '__main__':
     main(sys.argv[1:])
 
-del sys, join, etree
+del sys

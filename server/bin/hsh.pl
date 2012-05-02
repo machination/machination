3d99b1a6a6ffe018bb95a98b8eede47b1b314035
Colin Higgs <colin.higgs@ed.ac.uk>
2012-05-02 16:06:23 +0100

    shell now good for browsing contents

--- server/bin/hsh.pl
+++ server/bin/hsh.pl
@@ -6,12 +6,24 @@ use Machination::HPath;
 use Data::Dumper;
 use Getopt::Long;
 use Text::ParseWords;
+use Term::ANSIColor;
 
 $ENV{PERL_RL} = 'Zoid';
 eval 'use Term::ReadLine';
 die $@ if $@;
 
 Exception::Class::Base->Trace(1) if $ENV{DEBUG};
+
+# these work fairly well if you're colour blind (well not completely
+# or there would be no point...) and using a black background. Someday
+# they should be configurable.
+my $colors = {
+              container => 'bold bright_blue',
+              key => 'yellow',
+              value => 'bright_yellow',
+              title => 'bold bright_red',
+              undef => 'magenta',
+           };
 
 #my $client_type = "ha";
 my $config = "/home/eggy/svn/machination/trunk/test/machination/config.xml";
@@ -24,7 +36,7 @@ GetOptions(
            );
 
 # set up our list of commands
-my @cmds = qw(config pwd cd ls);
+my @cmds = qw(config pwd cd ls show);
 my %cmds = map {$_ => \&{"_".$_}, } @cmds;
 
 my $term = Term::ReadLine->new('Machination hsh');
@@ -62,19 +74,66 @@ sub _pwd {
 }
 
 sub _cd {
-  $pwd = __extend_path($pwd,$_[0]);
+  $pwd = __abs_path($_[0]);
   return $pwd;
 }
 
-sub __extend_path {
-  my ($base, $ext) = @_;
-  unless($ext=~/^\//) {
-    $base .= "/" unless($base=~/\/$/);
-    $ext = $base . $ext;
+sub __abs_path {
+  my ($p) = @_;
+  my $base = $pwd;
+  if(defined $p) {
+    if ($p=~/^\//) {
+      return $p;
+    } else {
+      $base .= "/" unless($base=~/\/$/);
+    }
+    $base = $base . $p;
   }
-  return $ext;
+  return $base;
 }
 
 sub _ls {
+  my ($path) = @_;
+  $path = __abs_path($path);
+  my $hp = Machination::HPath->new($ha, $path);
+  die "$path does not exist" unless $hp->id;
+  my $maxlen = 0;
+  my @cont;
+  my $sth = $ha->get_contents_handle
+    ($hp->id, ['machination:hc', keys %{$ha->all_types_info}]);
+  while (my $ent = $sth->fetchrow_hashref) {
+    my $name = $ent->{name};
+    my $type = $ha->type_name($ent->{type});
+    if($ent->{type} eq 'machination:hc') {
+      $name .= "/";
+      $name = colored([$colors->{container}], $name);
+    } else {
+      $name = colored([$colors->{key}],"$type:") .
+        colored([$colors->{value}],"$name");
+    }
+    $maxlen = length($name) if(length($name) > $maxlen);
+    push @cont, $name;
+  }
+  return join("\n",@cont);
+}
 
+sub _show {
+  my ($path) = @_;
+  $path = __abs_path($path);
+  my $hp = Machination::HPath->new($ha, $path);
+  die "$path does not exist" unless $hp->id;
+  my $d = Machination::HObject->new($ha, $hp->type_id, $hp->id)
+    ->fetch_data;
+  my @ret;
+  push @ret, colored([$colors->{title}], $path);
+  foreach my $f (sort keys %$d) {
+    my $str = "  " . colored([$colors->{key}],sprintf('%-15s',"$f:"));
+    if (defined $d->{$f}) {
+      $str .=  colored([$colors->{value}],$d->{$f});
+    } else {
+      $str .= colored([$colors->{undef}],'undef');
+    }
+    push @ret, $str;
+  }
+  return join("\n", @ret);
 }

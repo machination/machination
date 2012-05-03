ccc6d832a3452de5589134c979f3015e30a72f19
Colin Higgs <colin.higgs@ed.ac.uk>
2012-05-03 15:04:01 +0100

    starting to fix up python webclient

--- machination/webclient.py
+++ machination/webclient.py
@@ -1,23 +1,34 @@
-# WebClient module
-#import cPickle
 import pprint
 import sys
 from lxml import etree
-import urllib2
-import types
-#from machination.exceptions import *
-import machination.xmldata
+import urllib.request
+from machination.xmldata import from_xml, to_xml
+from machination import context
 
-class WebClient:
-    "OO interface to Machination WebClient"
 
-    def call(self,name,*args,**opts):
+class WebClient(object):
+    """Machination WebClient"""
+
+    def __init__(self, url, user):
+        self.url = url
+        self.user = user
+        self.encoding = 'utf-8'
+        self.l = context.logger
+
+    def call(self, name, *args):
         print self.user + " is calling " + name + " on " + self.url
         call_elt = etree.Element("r",call=name,user=self.user)
         for arg in args:
             call_elt.append(machination.xmldata.to_xml(arg))
         print etree.tostring(call_elt,pretty_print=True)
-        f=urllib2.urlopen(self.url,etree.tostring(call_elt))
+
+        # construct and send a request
+        r = urllib.request.Request\
+            (self.url,
+             etree.tostring(call_elt, encoding=self.encoding),
+             {'Content-Type':
+                  ' application/x-www-form-urlencoded;charset={}'.format(self.encoding)})
+        f=urllib.request.urlopen(r)
         return etree.parse(f)
 
     def help(self):

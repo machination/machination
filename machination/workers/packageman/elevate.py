e766486cc53890dc9162fa84cb9a5deae459e93e
Bruce Duncan <Bruce.Duncan@ed.ac.uk>
2012-06-11 12:16:04 +0100

    Protect code with "if main" test.
    Fixes "return outside of function" error.

--- machination/workers/packageman/elevate.py
+++ machination/workers/packageman/elevate.py
@@ -4,17 +4,21 @@ from machination import context
 
 # Launch a new process with elevation
 
-if len(sys.argv) != 3:
-    msg = "Failed to raise privileges: Wrong number of parameters supplied"
-    context.emsg(msg)
-    return msg
+def main():
+    if len(sys.argv) != 3:
+        msg = "Failed to raise privileges: Wrong number of parameters supplied"
+        context.emsg(msg)
+        return msg
 
-application = sys.argv[1]
-commandline = sys.argv[2]
+    application = sys.argv[1]
+    commandline = sys.argv[2]
 
-win32api.ShellExecute(0,
-                      'runas',
-                      application,
-                      commandline,
-                      '',
-                      1)
+    win32api.ShellExecute(0,
+                          'runas',
+                          application,
+                          commandline,
+                          '',
+                          1)
+
+if __name__ == '__main__':
+    main()

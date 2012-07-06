15842eb2b45e656df64c4577945946b5d8bac7bf
Colin Higgs <colin.higgs@ed.ac.uk>
2012-07-06 15:10:49 +0100

    CosignHandler now working

--- join-service.py
+++ join-service.py
@@ -6,62 +6,112 @@ import http.client
 import http.cookiejar
 import getpass
 
-def cosign_opener(login_url, cookie_file='cookies.txt'):
-    """Generate an OpenerDirector instance which can fetch cosign protected urls"""
-    cj = http.cookiejar.MozillaCookieJar(cookie_file)
-    cj.load(ignore_discard=True)
+class CosignHandler(urllib.request.BaseHandler):
+    """urllib.request style handler for Cosign protected URLs.
+
+    See http://weblogin.org
+
+    SYNOPSIS:
+
+    # Cosign relies on cookies.
+    cj = http.cookiejar.MozillaCookieJar('cookies.txt')
+
+    # If you've got one big program you'll probably want to keep the
+    # cookies in memory, but for lots of little programs we get single
+    # sign on behaviour by saving and looading to/from a file.
+    try:
+        # If this is the first script invocation there might not be a cookie
+        # file yet.
+        cj.load(ignore_discard=True)
+    except IOError:
+        pass
+
+    # We need an opener that handles cookies and any cosign redirects and
+    # logins.
     opener = urllib.request.build_opener(
-        urllib.request.HTTPCookieProcessor(cj)
+        urllib.request.HTTPCookieProcessor(cj),
+        # Here's the CosignHandler. Note that the login page for our
+        # cosign server is https://www.ease.ed.ac.uk/
+        CosignHandler('https://www.ease.ed.ac.uk/', cj)
         )
 
-    def splat(txt):
-        print(txt)
+    # Construct a request for the page we actually want
+    req = urllib.request.Request(
+        url='https://www.see.ed.ac.uk/~wwwuser2/bin/auth/licusers.cgi',
+        )
 
-    opener.splat = splat
+    # If all went well, res encapsulates the desired result, use res.read()
+    # to get at the data and so on.
+    res = opener.open(req)
 
-    return opener
-
-class CosignHandler(urllib.request.BaseHandler):
+    # Save the cookies for future programs (single sign on until they
+    # expire)
+    cj.extract_cookies(res,req)
+    cj.save(ignore_discard=True)
+    """
 
     def __init__(self, login_url, cj):
+        """Construct new CosignHandler.
+
+        Args:
+          login_url: URL of cosign login page. Used to figure out if we
+            have been redirected to the login page after a failed
+            authentication, and as the URL to POST to to log in.
+
+          cj: An http.cookiejar.CookieJar or equivalent. You'll need
+            something that implements the FileCookieJar interface if
+            you want to load/save cookies.
+        """
         super().__init__()
         self.login_url = login_url
         self.cj = cj
 
     def https_response(self, req, res):
+        """Handle https_response.
+
+        If the response is from the cosign login page (starts with
+        self.login_url) then log in to cosign and retry. Otherwise
+        continue as normal.
+        """
         if res.code == 200 and res.geturl().startswith(self.login_url + '?'):
-            print('been redirected to login page')
+            # Been redirected to login page.
+
+            # We'll need the cosign cookies later
             self.cj.extract_cookies(res, req)
-#            user = input('username: ')
-#            pwd = getpass.getpass()
-            user = 'splat'
-            pwd = 'frog'
+
+            # Grab a username and password.
+            user = input('username: ')
+            pwd = getpass.getpass()
             data = urllib.parse.urlencode({'login': user,
                                            'password': pwd})
+
+            # Construct a login POST request to the login page.
             req2 = urllib.request.Request(
-                res.geturl(),
+                self.login_url,
                 data.encode('iso-8859-1'),
-                {'Content-Type':
-                     'application/x-www-form-urlencoded;charset=%s' % 'iso-8859-1'}
                 )
+            # We need a different opener that doesn't have a CosignHandler.
             opener = urllib.request.build_opener(
                 urllib.request.HTTPCookieProcessor(cj)
                 )
+            # Try the login
             res2 = opener.open(req2)
+            # Cookies, cookies, cookies
+            cj.extract_cookies(res2, req2)
 
-            print(res2.code)
-            print(res2.geturl())
-            print(res2.read().decode('iso-8859-1'))
+            # We should be logged in, go back and get what was asked for
+            res = opener.open(req)
+
         return res
 
 
 if __name__ == '__main__':
 
     cj = http.cookiejar.MozillaCookieJar('cookies.txt')
-    cj.load(ignore_discard=True)
-    cj.save('lastcookies.txt', ignore_discard=True)
-    data = urllib.parse.urlencode({'login':'splat',
-                                   'password': 'frog'}).encode('utf-8')
+    try:
+        cj.load(ignore_discard=True)
+    except IOError:
+        pass
     opener = urllib.request.build_opener(
         urllib.request.HTTPCookieProcessor(cj),
         CosignHandler('https://www.ease.ed.ac.uk/', cj)
@@ -72,19 +122,6 @@ if __name__ == '__main__':
     res = opener.open(req)
     cj.extract_cookies(res,req)
     cj.save(ignore_discard=True)
-#    print(res.geturl())
-#    print(res.read().decode('utf-8'))
-    exit()
+    print(res.read().decode('utf-8'))
+    print('I got that from {} you know.'.format(res.geturl()))
 
-
-    data = urllib.parse.urlencode({'login':'splat',
-                                   'password': 'frog'}).encode('utf-8')
-    r = urllib.request.Request(
-        'https://www.ease.ed.ac.uk/',
-        data,
-        {'Content-Type':
-             'application/x-www-form-urlencoded;charset=%s' % 'utf-8'}
-        )
-    f = urllib.request.urlopen(r)
-    s = f.read().decode('utf-8')
-    print(s)

9838d14ac3fe76444d2c34c9699ea1e810a210d5
Colin Higgs <colin.higgs@ed.ac.uk>
2012-07-24 23:59:43 +0100

    separate out object creation from service join

--- join-service.py
+++ join-service.py
@@ -13,10 +13,12 @@ from lxml import etree
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description=__doc__)
-    parser.add_argument('os_id', nargs='?',
+    parser.add_argument('inst_id', nargs='?',
                         help='os_instance id')
     parser.add_argument('--service_id', '-s', nargs='?',
                         help='service id')
+    parser.add_argument('--location', '-l', nargs='?',
+                        help='parent hc')
     parser.add_argument('--openssl', nargs='?',
                         help='openssl command')
     parser.add_argument('--opensslcfg', nargs='?',
@@ -41,11 +43,16 @@ if __name__ == '__main__':
         # create an empty one so that the rest of the program will work
         service_elt = etree.Element('service')
 
-    # prefer os_id from args, then use context.get_id()
-    if args.os_id:
-        os_id = args.os_id
+    # prefer inst_id from args, then use context.get_id()
+    if args.inst_id:
+        inst_id = args.inst_id
     else:
-        os_id = context.get_id(service_id)
+        inst_id = context.get_id(service_id)
+
+    # default location
+    location = '/system/os_instances'
+    if args.location:
+        location = args.location
 
     # defaults for openssl binary and config file
     openssl = 'openssl'
@@ -72,7 +79,7 @@ if __name__ == '__main__':
         except IndexError:
             pass
 
-    print('os_id: {}, service_id: {}'.format(os_id, service_id))
+    print('inst_id: {}, service_id: {}'.format(inst_id, service_id))
     print('openssl: {}, opensslcfg: {}'.format(openssl, opensslcfg))
 
     certdir = os.path.join(
@@ -88,10 +95,11 @@ if __name__ == '__main__':
     print('genkey: {}'.format(cmd))
     pending_keyfile = os.path.join(certdir, 'pending.key')
     print('Generating new key')
-    key = subprocess.check_output(cmd)
-#    key = b'splat'
-    with machination.create_secret_file(pending_keyfile,'wb') as f:
-        f.write(key)
+#    key = subprocess.check_output(cmd)
+#    with machination.create_secret_file(pending_keyfile,'wb') as f:
+#        f.write(key)
+    with open(pending_keyfile) as f:
+        key = f.read()
 
     # generate the csr
     cmd = []
@@ -129,6 +137,23 @@ if __name__ == '__main__':
     with machination.create_secret_file(pending_csrfile,'wb') as f:
         f.write(csr)
 
+    path = '{}/os_instance:{}'.format(location, inst_id)
+    sysname = platform.system()
+    if sysname == 'Windows':
+        if 'PROGRAMFILES(X86)' in os.environ:
+            bitness = 64
+        else:
+            bitness = 32
+        major = platform.win32_ver()[0]
+        minor = platform.win32_ver()[2]
+    elif sysname == 'Linux':
+        pass
+    os_id = wc.call('OsId', sysname, major, minor, bitness)
+    # try to create the object
+    wc.call('Create', path, {'os_id': os_id})
+
+    exit()
+
     answer = wc.call("JoinService", csr.decode('utf8'), None, False)
     if isinstance(answer, dict):
         # Didn't get all the way to a cert for some reason
@@ -164,5 +189,5 @@ if __name__ == '__main__':
         if e.errno == 2:
             pass
     # Create new one.
-    with open(certfile'wb') as f:
+    with open(certfile, 'wb') as f:
         f.write(cert)

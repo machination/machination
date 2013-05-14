5ca951754cfdf7753430624495ff5a49f9a4e0ae
Alasdair King <smashinimo@gromit.see.ed.ac.uk>
2013-05-14 09:46:18 +0100

    update: ammended initual printer worker removing the template based on shortcut
    add: printer dict with a list in each key,value as options for
    printui.exe
    tiday: removed all shortcut code in add(): and remove():

--- machination/workers/printer/__init__.py
+++ machination/workers/printer/__init__.py
@@ -13,22 +13,15 @@ import stat
 l = context.logger
 
 class Worker(object):
-    windowstyles = {"normal": 1,
-                    "max": 3,
-                    "min": 7}
+        #printer options to printui, defalt construct
+        printer = {"printerui":(r"/in", "/u"), #check optins
+               "base_name":(r"/b","bname"),
+               "printer_name":(r"/x","/n","pname"),
+               "net_addr":(r"/r","addr"),
+               "inf":(r"/if","/f","inf"),
+               "driver":(r"/l","driv"),
+               "modal":(r"/m","modal")}
 
-    s_props = {"name": "",
-               "target": "",
-               "windowStyleName": "",
-               "window": 1,
-               "iconFile": "",
-               "iconNumber": "",
-               "icon": "",
-               "arguments": "",
-               "description": "",
-               "workingDir": "",
-               "destination": "",
-               "folderName": ""}
 
     def __init__(self):
         self.name = self.__module__.split('.')[-1]
@@ -58,43 +51,9 @@ class Worker(object):
         res = etree.Element("wu",
                             id=work.attrib["id"])
 
-        # Parse out shortcut properties from XML
-        id = work[0].attrib["id"]
-        for elem in work[0]:
-            s_props[elem.tag] = elem.text
-
-        if not s_props["iconFile"]:
-            s_props["iconFile"] = s_props["target"]
-        if not s_props["iconNumber"]:
-            s_props["iconNumber"] = 0
-        s_props["icon"] = ",".join(s_props["iconFile"],
-                                   s_props["iconNumber"])
-        s_props["name"] += ".lnk"
-        if s_props["WindowStyleName"]:
-            s_props["window"] = windowstyle[windowStyleName]
-
-        if not target:
-            msg = "Must provide target for shortcut " + id
-            res.attrib["status"] = "error"
-            res.attrib["message"] = msg
-            l.emsg(msg)
-            return res
-
-        dest = self.shell.SpecialFolders(s_props["destination"])
-
-        if s_props["folderName"]:
-            dest = os.path.join(dest, s_props["folderName"])
-            os.makedirs(dest)
-
-        filename = os.path.join(dest, s_props["name"])
-        link = self.shell.CreateShortcut(filename)
-        link.TargetPath = s_props["target"]
-        link.WindowStyle = s_props["window"]
-        link.IconLocation = s_props["icon"]
-        link.WorkingDirectory = s_props["workingDir"]
-        link.Arguments = s_props["arguments"]
-        link.Description = s_props["description"]
-        link.Save()
+        #do printer additions in here
+        #defins subprocsess.Popen and use it with printui.exe and the
+        #options form printer{} 
 
         res.attrib["status"] = "success"
         return res
@@ -116,36 +75,7 @@ class Worker(object):
                             id=work.attrib["id"])
 
         id = work[0].attrib["id"]
-        try:
-            keepfolder = work[0].attrib["keepfolder"]
-        except:
-            keepfolder = true
 
-        for elem in work[0]:
-            s_props[elem.tag] = elem.text
-
-        s_props["name"] += ".lnk"
-
-        dest = self.shell.SpecialFolders(s_props["destination"])
-        if s_props["folderName"]:
-            dest = os.path.join(dest, s_props["folderName"])
-        filename = os.path.join(dest, s_props["name"])
-
-        os.remove(filename)
-
-        res.attrib["status"] = success
-
-        if not keepfolder:
-            shutil.rmtree(dest,
-                          ignore_errors=false,
-                          onerror=self.handleRemoveReadonly)
+        #do removal here
 
         return res
-
-    def handleRemoveReadOnly(self, func, path, exc):
-        excvalue = exc[1]
-        if func in (os.rmdir, os.remove) and excvalue.errno == errno.EACCESS:
-            os.chmod(path, stat.S_IRWXU | stat.S_IRWXG | stat.IRWXO)
-            func(path)
-        else:
-            raise

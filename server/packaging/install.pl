5e68ae3e0cf1992d4aac206bf5ee3b114f298c03
Colin Higgs <colin.higgs@ed.ac.uk>
2013-11-13 21:05:15 +0000

    deb: install, perms, owners apache config working

--- server/packaging/install.pl
+++ server/packaging/install.pl
@@ -3,7 +3,7 @@
 use strict;
 use warnings;
 
-use Getopt::Long;
+use Getopt::Long qw(GetOptionsFromString);
 use File::Find;
 use File::Path qw(make_path);
 use File::Spec;
@@ -11,6 +11,11 @@ use File::Copy;
 use B qw(svref_2object);
 use Text::Template;
 use Pod::Usage;
+use Regexp::Common;
+use File::Basename;
+use Cwd qw(abs_path);
+use Data::Dumper;
+$Data::Dumper::Indent=1;
 
 my $tgt_root="";
 my $src_root="";
@@ -21,32 +26,72 @@ my $debug;
 my $doit = 1;
 my $manifest = 1;
 my $makepkg;
-my $mach_version = qx'git describe --tags --long | sed \'s/-\\([0-9]\\+\\)-.*$/\\.\\1/\'';
-chomp $mach_version;
 my $pkgname = "machination-server";
+my $scriptdir = File::Basename::dirname(abs_path(__FILE__));
+my $files_tmpl_file = "$scriptdir/files.tmpl";
+my $postinst_tmpl_file = "$scriptdir/machination-server.postinst.tmpl";
+my $tgt_distro;
+my $apache_conf_dir;
 
 GetOptions(
            "tgt_root=s"=>\$tgt_root,
            "src_root=s"=>\$src_root,
            "debug"=>\$debug,
-           "doit"=>\$doit,
+           "doit=i"=>\$doit,
            "manifest"=>\$manifest,
            "makepkg=s"=>\$makepkg,
            "pkgname=s"=>\$pkgname,
+           "tgt_distro=s"=>\$tgt_distro,
+           "files_template=s"=>\$files_tmpl_file,
+           "postinst_template=s"=>\$postinst_tmpl_file,
+           "apache_conf_dir=s"=>\$apache_conf_dir,
            ) || pod2usage(-verbose=>0);
 
 my $system_perllib_dir = "/usr/share/perl5";
 my $system_bin_dir = "/usr/bin";
 
+my $files_vars =
+  {
+   system_perllib_dir => $system_perllib_dir,
+   system_bin_dir => $system_bin_dir,
+   mach_config_dir => $mach_config_dir,
+   mach_lib_dir => $mach_lib_dir,
+   mach_log_dir => $mach_log_dir,
+  };
+if(!defined $apache_conf_dir) {
+  if($tgt_distro eq "debian") {
+    $apache_conf_dir = "/etc/apache2/conf-available";
+    $files_vars->{apache_user} = "www-data";
+  } elsif($tgt_distro eq "redhat") {
+    $apache_conf_dir = "/etc/httpd/conf.d";
+    $files_vars->{apache_user} = "apache";
+  } else {
+    die "Can't figure out where apache config should go";
+  }
+}
+$files_vars->{mach_apache_conf_file} = "$apache_conf_dir/${pkgname}.conf";
+
+my $files_tmpl = Text::Template->new(SOURCE=>$files_tmpl_file)
+  or die "could not construct template from $files_tmpl_file";
+my @files_lines = split("\n", $files_tmpl->fill_in(HASH=>$files_vars));
+my @parsed_lines;
+my $n = 1;
+foreach (@files_lines) {
+  # clean up the input
+  s/\#.*$//; # remove comments
+  s/\s+$//; # remove white space at the end of a line
+  next if /^$/; # ignore empty lines
+
+  my ($ret, $args, $opts) = parse_files_opts($_);
+  die "Error at $files_tmpl_file line $n" unless($ret);
+  push @parsed_lines, {args=>$args, opts=>$opts};
+  $n++;
+}
+
 # wrap instrumentation around some functions
 my $copy = wrap(\&File::Copy::cp);
 my $make_path = wrap(\&File::Path::make_path);
 my $ln = wrap(\&ln);
-
-my $vars =
-  {
-   mach_version=>$mach_version,
-  };
 
 my $cmd = $ARGV[0];
 no strict "refs";
@@ -57,13 +102,65 @@ if(defined(&{"cmd_$cmd"})) {
 }
 exit;
 
+sub parse_files_opts {
+  my $str = shift;
+  my $opts = {};
+  my ($ret, $args) = GetOptionsFromString
+    (
+     $str,
+     $opts,
+     "config=s",
+     "perms=s",
+     "owner=s"
+     );
+  die
+  return ($ret,$args,$opts);
+}
+
+sub cmd_deb_conffiles {
+  for my $info (@parsed_lines) {
+    next unless(exists $info->{opts}->{config});
+    print join("\n", @{$info->{args}}) . "\n";
+  }
+}
+
+sub cmd_deb_perms {
+  for my $info (@parsed_lines) {
+    if (exists $info->{opts}->{perms}) {
+      print "chmod " . $info->{opts}->{perms} . "," . join(",", map {tgtdir($_)} @{$info->{args}}) . "\n";
+      chmod(oct($info->{opts}->{perms}), map {tgtdir($_)} @{$info->{args}});
+    }
+  }
+}
+
+sub cmd_deb_postinst {
+  my @chown_lines;
+  for my $info (@parsed_lines) {
+    if (exists $info->{opts}->{owner}) {
+      foreach my $f (@{$info->{args}}) {
+        my ($owner, $group) = split(":",$info->{opts}->{owner});
+        print "chown $owner $f\n";
+        push @chown_lines, "chown $owner $f";
+        push @chown_lines, "chgrp $group $f" if(defined $group);
+      }
+    }
+  }
+
+  my $post_tmpl = Text::Template->new(SOURCE=>$postinst_tmpl_file)
+    or die "could not construct template from $postinst_tmpl_file";
+  my $script = $post_tmpl->fill_in(HASH=>{chown_lines=>\@chown_lines});
+  print $script;
+}
+
+sub cmd_deb_owner {
+}
 
 sub cmd_install {
   # install perl libs
-  print "installing to " . tgtdir() . "...\n";
+  print "installing to " . tgtdir() . " from " . srcdir() . "...\n";
   -d tgtdir() or $make_path->(tgtdir());
   find(find_install(tgtdir($system_perllib_dir),
-                    {remove_leading=>1,
+                    {trim=>srcdir("lib"),
                      ignore=>"~\$"}),
        srcdir("lib"));
 
@@ -84,7 +181,7 @@ sub cmd_install {
 
   # commands
   find(find_install(tgtdir($system_bin_dir),
-                    {remove_leading=>1,
+                    {trim=>srcdir("bin"),
                      ignore=>"~\$"}),
        srcdir("bin"));
 
@@ -122,20 +219,36 @@ sub cmd_rpm_config_files {
 
 }
 
+sub formatstr {
+  my $str = shift;
+  my $hash = shift;
+
+  $str =~ s/($RE{balanced}{-parens=>'{}'})/findsub($1, $hash)/eg;
+
+  return $str;
+}
+
+sub findsub {
+  my $str = shift;
+  my $hash = shift;
+  # strip brackets
+  my $newstr = substr($str, 1, length($str) - 2);
+  if (! exists $hash->{$newstr}) {
+    warn "No substitution found for $str -- leaving in place";
+    return $str;
+  }
+  return $hash->{$newstr};
+}
+
 sub find_install {
   my $tgt = shift;
   my $opts = shift;
+  $opts->{trim} = srcdir() unless exists $opts->{trim};
   return sub {
-    my $dir = $File::Find::dir;
-#    print "checking $dir\n";
-    if ($opts->{remove_leading}) {
-      my @dirs = File::Spec->splitdir($dir);
-      for(1..$opts->{remove_leading}) {shift @dirs}
-      $dir = File::Spec->catdir(@dirs);
-    }
+    my $dir = File::Spec->rel2abs($File::Find::dir);
+    $dir =~ s/^$opts->{trim}//;
     return if(/$opts->{ignore}/);
-    my $tgt_dir;
-    $tgt_dir = $tgt;
+    my $tgt_dir = File::Spec->catdir($tgt,$dir);
     if(-d "$_") {
       $make_path->(File::Spec->catfile($tgt_dir,$_));
     } else {
@@ -169,14 +282,14 @@ sub ln {
 }
 
 sub srcdir {
-  return dirname($src_root, @_);
+  return addroot($src_root, @_);
 }
 
 sub tgtdir {
-  return dirname($tgt_root,@_);
+  return addroot($tgt_root,@_);
 }
 
-sub dirname {
+sub addroot {
   my $root = shift;
   my @dirs = @_;
   unshift(@dirs, $root ) if(defined $root && $root ne "");
